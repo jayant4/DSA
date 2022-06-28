c18cc21e00cffd3bf56e87f6d41bb6ea39f9139e
Jayant Sarkar <jayant.sarkar@in.abb.com>
2022-06-28 11:23:28 +0530

    Two_Sum

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,3 +1,15 @@
 '''
 Calculate 2 sum
+Given an array of numbers, and a number return a pair of numbers which add up to the given number
 '''
+
+arr:int = [1, 2, 3, 4,int(-5), 6, 7, 8, 9, 10]
+sum = 5
+output = []
+def TwoSum():
+    for i in range(0,len(arr)):
+        for j in range(0,len(arr) -1):
+            if((i + j) == sum):
+                output.append((i,j))
+TwoSum()
+print(output)
