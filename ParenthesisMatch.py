635ef4099b3609e66c9db73b998f68de97ffd613
Jayant Sarkar <jayant.sarkar@in.abb.com>
2022-06-28 11:51:50 +0530

    ParenthesisMatch

---
+++ ParenthesisMatch.py
@@ -0,0 +1,25 @@
+'''
+Given a set of parenthesis, check if they match properly
+can validate only () [] {}
+'''
+
+
+strParenthesis = '(){}[]'
+
+dict= {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+}
+
+
+def MatchParenthesis(input: str) -> bool:
+    pass
+    # flag = False
+    # for i in range(0, len(strParenthesis)-1):
+
+       
+            
+    # return flag
+
+print(MatchParenthesis(strParenthesis))
