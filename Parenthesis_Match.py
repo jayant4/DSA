ac66ad11f6a0bcf29df6c76325d6bf0697688b12
Jayant <jayantsarkar89@gmail.com>
2022-07-06 23:28:47 +0530

    Parenthesis match

--- Parenthesis_Match.py
+++ Parenthesis_Match.py
@@ -29,70 +29,38 @@ s consists of parentheses only '()[]{}'.
 '''
 
 
-dict = {
-    '(': ')',
-    '[': ']',
-    '{': '}'
-}
+mystack = []
+inputString = '{}}({}[])'
+
+
 
 
 def checkBrackets(inputStr: str) -> bool:
-    '''
-    working solution
-    but does not work for nested brackets like {[]} returns false
-    '''
-
-    for i in range(0, len(inputStr)-1):
-        if(inputStr[i] == '('):
-            if(inputStr[i+1] != dict['(']):
-                return False
-        elif(inputStr[i] == '['):
-            if(inputStr[i+1] != dict['[']):
-                return False
-        elif(inputStr[i] == '{'):
-            if(inputStr[i+1] != dict['{']):
-                return False
-    return True
-
-
-inputString = '(){}[]'
-
-# if(checkBrackets(inputString)):
-#     print('Valid parenthesis')
-# else:
-#     print('Invalid parenthesis')
-
-
-mystack = []
-inputString2 = '{({}[])}'
-
-
-
-
-def checkBrackets2(inputStr: str) -> bool:
     doesMatch: bool = False
-    for i in range(0, len(inputStr)-1):
-        if inputStr[i] in dict.keys():
+    for i in range(0, len(inputStr)):
+        top = 0
+        if(len(mystack)<=0):
+            top = "#"
+        else:
+            top = mystack[len(mystack)-1]
+            
+        if(top == "(" and inputStr[i] == ")"):
+            mystack.pop()
+        elif(top == "[" and inputStr[i] == "]"):
+            mystack.pop()
+        elif(top == "{" and inputStr[i] == "}"):
+            mystack.pop()
+        else:
             mystack.append(inputStr[i])
-        
-        if inputStr[i] in dict.values():
-            x = mystack.pop()
-            if(GetKey(inputStr[i]) == x):
-                doesMatch = True
-            else:
-                return False
     
-    return doesMatch
+    return len(mystack) == 0
         
-def GetKey(val):
-   for key, value in dict.items():
-      if val == value:
-         return key
 
-if(checkBrackets2(inputString2)):
+if(checkBrackets(inputString)):
     print('Valid parenthesis')
 else:
     print('Invalid parenthesis')
 
 
-
+"""
+"""
