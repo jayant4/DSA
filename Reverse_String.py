16152935af8e9cf760d5ec830f6f66603503f525
Jayant <jayantsarkar89@gmail.com>
2022-07-06 23:48:39 +0530

    Parenthesis match reverse order

--- Reverse_String.py
+++ Reverse_String.py
@@ -12,4 +12,15 @@ def ReverseString(inputString : str) -> str:
         pass
 
     return reverseStr
-print(ReverseString(inputString))
+
+def ReverseString2(inputString : str) -> str:
+    reverseStr = ""
+    for i in range(len(inputString)-1,-1,-1):
+        reverseStr += inputString[i]
+
+    # for i in inputString:
+    #     reverseStr += myStack.pop()
+        pass
+
+    return reverseStr
+print(ReverseString2(inputString))
