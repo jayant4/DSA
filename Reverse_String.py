2f9b290f4aad12361ca7c7c9f2a38f51a9071e95
Jayant <jayantsarkar89@gmail.com>
2022-07-06 23:49:56 +0530

    Reverse string reverse loop

--- Reverse_String.py
+++ Reverse_String.py
@@ -17,10 +17,7 @@ def ReverseString2(inputString : str) -> str:
     reverseStr = ""
     for i in range(len(inputString)-1,-1,-1):
         reverseStr += inputString[i]
-
-    # for i in inputString:
-    #     reverseStr += myStack.pop()
-        pass
-
     return reverseStr
+
+    
 print(ReverseString2(inputString))
